2f41b51808be77d576e6194db3ebb1098e3ea3b3
miller <miller_maxwell@outlook.com>
2017-05-28 15:51:16 +0800

    add download data script

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -c -O train.zip "https://publicqn.saikr.com/3f96bef8dcbbf57605db3f5e79d5384e1495175270342.zip?attname=dsjtzs_txfz_training.txt.zip"
+wget -c -O test.zip "https://publicqn.saikr.com/b4d50fbf55ed071e2bcdbf6448ee5caa1495681365947.zip?attname=dsjtzs_txfz_test1.txt.zip"
+unzip train.zip
+unzip test.zip
+rm -f train.zip test.zip
