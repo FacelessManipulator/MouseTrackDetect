5d74aa0e6bc4b817caea51c309c7b3a08d22e3b3
miller <miller_maxwell@outlook.com>
2017-06-04 16:47:40 +0800

    add speed plot function

--- pre.py
+++ pre.py
@@ -4,7 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--set', help='human | machine', default='human')
-parser.add_argument('--attr', help='track | acceleration | speed', default='track')
+parser.add_argument('--attr', help='track | acceleration | speed | x | y', default='track')
 parser.add_argument('--begin', type=int, help='the begin index of the set to display', default=0)
 parser.add_argument('--count', type=int, help='number of data to display', default=10)
 
@@ -25,10 +25,20 @@ def get_track(s):
 def plot_track(axes, track):
     index, start, track, des, flag = get_track(track)
     axes.plot(track[:,0],track[:,1], '-b')
+
+def plot_x(axes, track):
+    index, start, track, des, flag = get_track(track)
+    axes.plot(track[:,-1],track[:,0], '-b')
+
+def plot_y(axes, track):
+    index, start, track, des, flag = get_track(track)
+    axes.plot(track[:,-1],track[:,1], '-b')
     
 def plot_speed(axes, track):
     index, start, track, des, flag = get_track(track)
-    axes.plot(track[:,0],track[:,1], '-b')
+    sp = get_speed(track)
+    axes.plot(track[1:-1, 2],sp[:,0], '-b')
+    axes.plot(track[1:-1, 2],sp[:,1], '-g')
     
 def plot_acceleration(axes, track):
     index, start, track, des, flag = get_track(track)
@@ -36,6 +46,23 @@ def plot_acceleration(axes, track):
     axes.plot(track[1:-1, 2],a[:,0], '-b')
     axes.plot(track[1:-1, 2],a[:,1], '-g')
     
+def get_speed(track, bias=1):
+    '''
+    此函数用于获得速度，至少需要三个点，具体公式是解三个点用速度表示时的方程组
+    这里默认时间偏差+1，是为了防止同一时间出现了两个点造成的inf
+    固有偏差影响，视平均时间差不同，速度总体上大约缩小了0.25%
+    '''
+    assert track.shape[0] >= 3
+    B_sub_A = track[1:-1] - track[:-2]
+    C_sub_A = track[2:] - track[:-2]
+    C_sub_B = track[2:] - track[1:-1]
+    sp = np.multiply((C_sub_B[:,:-1] / np.square(C_sub_B[:,-1]+bias).reshape((-1,1)) + \
+        B_sub_A[:,:-1] / np.square(B_sub_A[:,-1]+bias).reshape((-1,1))) ,
+        (B_sub_A[:,-1].reshape((-1,1))+bias))
+    sp = np.multiply(sp, (C_sub_B[:,-1].reshape((-1,1))+bias))
+    sp = sp/(C_sub_A[:,-1].reshape((-1,1))+bias)
+    return sp
+
 def get_acceleration(track):
     '''
     此函数用于获得加速度，至少需要三个点，具体公式是解三个点用加速度表示时的方程组
@@ -53,7 +80,8 @@ def get_acceleration(track):
 
 def show_next(data, fig, attr):
     i = -1
-    func = {'track':plot_track,'acceleration':plot_acceleration,'speed':plot_speed}
+    func = {'track':plot_track,'acceleration':plot_acceleration,'speed':plot_speed,
+           'x':plot_x, 'y':plot_y}
     def onclick(event):
         nonlocal i
         if event is None or event.button == 1:
@@ -63,7 +91,7 @@ def show_next(data, fig, attr):
         fig.clf()
         ax = fig.add_subplot(111)
         func[attr](ax, data[i])
-        fig.suptitle('cur:%d'%i, fontsize=12)
+        fig.suptitle('cur:%s-%s-%d'%(opt.set,opt.attr, i), fontsize=12)
         fig.canvas.draw()
     return onclick
     
